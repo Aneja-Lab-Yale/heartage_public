7ace613352f5a6db79c9dc89dc304fb7ce00416a
Crystal <crystal.cheung@yale.edu>
2023-04-06 19:45:51 -0400

    switched to mse

--- replicate_cnn.py
+++ replicate_cnn.py
@@ -121,7 +121,7 @@ optimizer = tf.keras.optimizers.Adam(use_ema=True)
 
 #regression
 #loss= tf.keras.losses.CategoricalCrossentropy(name='loss')
-loss = tf.keras.losses.MeanAbsoluteError(name='loss')
+loss = tf.keras.losses.MeanSquaredError(name='loss')
 # mean squared error (regression)
 # uses tf.keras... function to be the loss
 #met = [tf.keras.metrics.CategoricalAccuracy(name='accuracy')]
@@ -459,9 +459,9 @@ np.savetxt(project_root + 'results/age_predictions_reg_' + detail +'.csv', y_pre
 plt.figure(figsize=(10,8))
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='val')
-plt.title('MAE Loss '+ detail)
+plt.title('MSE Loss '+ detail)
 plt.xlabel('Epoch')
-plt.ylabel('MAE Loss')
+plt.ylabel('MSE Loss')
 plt.ylim([0, 50])
 plt.legend()
 #plt.show()
