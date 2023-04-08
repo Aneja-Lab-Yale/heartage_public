6a7ab97a3547594ed4b5be3452e7469f36700b7f
Crystal <crystal.cheung@yale.edu>
2023-04-08 15:06:13 -0400

    confusion matrix, mae loss, smaller age bins

--- replicate_cnn.py
+++ replicate_cnn.py
@@ -32,7 +32,7 @@ import seaborn as sns
 #mask_path = '/Users/Crystal/Desktop/College/PMAE/Thesis/Code/Heart_segmentations/'
 
 project_root = '/home/crystal_cheung/'
-detail = 'apr8_mae_waug'
+detail = 'apr8_mae_waug_2yr_bins'
 def callbacks_model(model_save_path,
                     csv_log_file,
                     patience,
@@ -422,18 +422,22 @@ age_class_ex = []
 for i in range(len(y_expected)):
     if y_expected[i] <= 57:
         age_bin = 0
-    elif 57 < y_expected[i] <= 60:
+    elif 57 < y_expected[i] <= 59:
         age_bin = 1
-    elif 60 < y_expected[i] <= 63:
+    elif 59 < y_expected[i] <= 61:
         age_bin = 2
-    elif 63 < y_expected[i] <= 66:
+    elif 61 < y_expected[i] <= 63:
         age_bin = 3
-    elif 66 < y_expected[i] <= 69:
+    elif 63 < y_expected[i] <= 65:
         age_bin = 4
-    elif 69 < y_expected[i] <= 72:
+    elif 65 < y_expected[i] <= 67:
         age_bin = 5
-    elif 72 < y_expected[i]:
+    elif 67 < y_expected[i] <= 69:
         age_bin = 6
+    elif 69 < y_expected[i] <= 71:
+        age_bin = 7
+    elif 71 < y_expected[i]:
+        age_bin = 8
 
     age_class_ex.append(age_bin)
 
@@ -441,18 +445,22 @@ age_class_pred = []
 for i in range(len(y_predicted)):
     if y_predicted[i] <= 57:
         age_bin = 0
-    elif 57 < y_predicted[i] <= 60:
+    elif 57 < y_predicted[i] <= 59:
         age_bin = 1
-    elif 60 < y_predicted[i] <= 63:
+    elif 59 < y_predicted[i] <= 61:
         age_bin = 2
-    elif 63 < y_predicted[i] <= 66:
+    elif 61 < y_predicted[i] <= 63:
         age_bin = 3
-    elif 66 < y_predicted[i] <= 69:
+    elif 63 < y_predicted[i] <= 65:
         age_bin = 4
-    elif 69 < y_predicted[i] <= 72:
+    elif 65 < y_predicted[i] <= 67:
         age_bin = 5
-    elif 72 < y_predicted[i]:
+    elif 67 < y_predicted[i] <= 69:
         age_bin = 6
+    elif 69 < y_predicted[i] <= 71:
+        age_bin = 7
+    elif 71 < y_predicted[i]:
+        age_bin = 8
 
     age_class_pred.append(age_bin)
 
@@ -465,8 +473,8 @@ results.append(r2)
 
 cm = confusion_matrix(age_class_ex, age_class_pred)
 age_df = pd.DataFrame(cm,
-                     index = ['<=57','58-60','61-63','64-66','67-69','70-72','73+'],
-                     columns = ['<=57','58-60','61-63','64-66','67-69','70-72','73+'])
+                     index = ['57-','58/59','60/61','62/63','64/65','66/67','68/69','70/71','71+'],
+                     columns = ['57-','58/59','60/61','62/63','64/65','66/67','68/69','70/71','71+'])
 
 corr, _ = pearsonr(age_class_ex, age_class_pred)
 model_metrics.append('r')
