3c241946b61712267d40849793a3c7cf7bef204b
Crystal <crystal.cheung@yale.edu>
2023-04-17 13:39:52 -0400

    saved training IDs to csv

--- subanalysis.py
+++ subanalysis.py
@@ -11,24 +11,26 @@ heart_volumes = pd.read_csv(project_root + '/results/heart_sizes.csv',header=Non
 expected_age_bins = pd.read_csv(project_root + 'results/age_expected_bin_'+detail+'.csv',header=None)
 predicted_age_bins = pd.read_csv(project_root + 'results/age_predictions_bin_'+detail+'.csv',header=None)
 full_list_gender = pd.read_csv(project_root + 'data/gender.csv',header=None)
-full_list_smoke = pd.read_csv(project_root + 'data/smoke.csv',header = None)
+
 
 test_IDs = np.asarray(test_IDs)
 heart_volumes = np.asarray(heart_volumes)
 full_list_gender = np.asarray(full_list_gender)
 expected_age_bins = np.asarray(expected_age_bins)
 predicted_age_bins = np.asarray(predicted_age_bins)
-full_list_smoke = np.asarray(full_list_smoke)
+
+train_IDs = np.load(project_root+"/results/train_ID_apr4_mae_waug.npy")
+train_IDs = np.asarray(train_IDs)
+np.savetxt(project_root + "/results/train_IDs_apr17.csv", train_IDs, delimiter=",",fmt='%s')
 
 test_volumes = []
 gender = []
-pack_year = []
+
 for id in range(len(test_IDs)):
     for patient in range(len(heart_volumes)):
         if test_IDs[id] == heart_volumes[patient,0]:
             test_volumes.append(heart_volumes[patient,1])
             gender.append(full_list_gender[patient, 1])
-            pack_year.append(full_list_smoke[patient,1])
         else:
             continue
         break
@@ -36,13 +38,12 @@ for id in range(len(test_IDs)):
 test_volumes = np.asarray(test_volumes)
 test_data = np.column_stack((test_IDs, test_volumes))
 test_data = np.column_stack((test_data, gender))
-test_data = np.column_stack((test_data, pack_year))
 test_data = np.column_stack((test_data, expected_age_bins))
 test_data = np.column_stack((test_data, predicted_age_bins))
 
 bin_diff = []
 for id in range(len(test_data)):
-    age_diff = test_data[id,4] - test_data[id,5]
+    age_diff = test_data[id,3] - test_data[id,4]
     bin_diff.append(age_diff)
 
 test_data = np.column_stack((test_data, bin_diff))
@@ -52,9 +53,9 @@ sorted_test_data = test_data[sorted_indices]
 
 over_under = []
 for i in range(len(sorted_test_data)):
-    if sorted_test_data[i,6] < 0:
+    if sorted_test_data[i,5] < 0:
         over_under.append('over')
-    elif sorted_test_data[i,6] == 0:
+    elif sorted_test_data[i,5] == 0:
         over_under.append('accurate')
     else:
         over_under.append('under')
@@ -62,7 +63,7 @@ for i in range(len(sorted_test_data)):
 sorted_test_data = np.column_stack((sorted_test_data, over_under))
 
 heartage_dict = {'Patient ID': sorted_test_data[:,0], 'Heart size': sorted_test_data[:,1],'Gender':sorted_test_data[:,2],
-                 'Pack/year':sorted_test_data[:,3],'Expected age bin':sorted_test_data[:,4],'Predicted age bin':sorted_test_data[:,5],
-                 'Bin difference':sorted_test_data[:,6],'Over-under':sorted_test_data[:,7]}
+                 'Expected age bin':sorted_test_data[:,3],'Predicted age bin':sorted_test_data[:,4],
+                 'Bin difference':sorted_test_data[:,5],'Over-under':sorted_test_data[:,6]}
 df = pd.DataFrame(heartage_dict)
 df.to_csv(project_root + 'results/sorted_test_data.csv',index=False)
